5076ba5c8d356e235d8a0b96c19285b4ee85a3f1
Mario A Ramirez <mario.ramirezorihuela@postgrad.manchester.ac.uk>
2014-03-21 11:52:41 +0000

    Sorting functionality added

--- src/comp61542/__init__.py
+++ src/comp61542/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+import os
 
-app = Flask(__name__)
+JQUERY_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), './jquery')
+print(JQUERY_DIR)
+app = Flask(__name__,static_folder=JQUERY_DIR)
 
 from comp61542 import views
